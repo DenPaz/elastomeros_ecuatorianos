7d10303075547218531b7e72af9f0146d5ad95e2
Giovanni <dppazlopez@gmail.com>
2025-11-03 14:29:35 -0300

    fix: remove unused signals import and delete signals.py file

--- apps/store/config.py
+++ apps/store/config.py
@@ -5,6 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class StoreConfig(AppConfig):
     name = "apps.store"
     verbose_name = _("Store")
-
-    def ready(self):
-        import apps.users.signals  # noqa: F401, PLC0415
