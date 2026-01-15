64dc6630cc78ce840f1c35fbf8316019f96b80b6
Dennis Paz <dppazlopez@gmail.com>
2026-01-15 19:06:32 -0300

    Fix product image upload path to include 'products/' prefix

--- apps/products/utils.py
+++ apps/products/utils.py
@@ -1,2 +1,2 @@
 def product_image_upload_to(instance, filename):
-    return f"products/{instance.variant.product.slug}/{filename}"
+    return f"products/products/{instance.variant.product.slug}/{filename}"
