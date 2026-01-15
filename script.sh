2e273485d3e476ad23d75f8f8e5535f2f5aff74e
Dennis Paz <dppazlopez@gmail.com>
2026-01-15 15:02:40 -0300

    Refactor script.sh to remove unnecessary --clear flag from seed_sample_products command

--- script.sh
+++ script.sh
@@ -4,9 +4,10 @@ uv run manage.py reset_db --noinput
 uv run manage.py clear_cache
 uv run manage.py clean_pyc
 find . -name '__pycache__' -type d -exec rm -r {} +
+rm -rf media/*
 uv run manage.py makemigrations
 uv run manage.py migrate
 uv run manage.py update_site_domain_and_name
 uv run manage.py create_test_users
-uv run manage.py seed_sample_products --clear
+uv run manage.py seed_sample_products
 uv run manage.py runserver
