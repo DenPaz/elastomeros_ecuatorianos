b51c6210f4ac9f117f521fd9a5d33f576d63ca2d
Dennis Paz <dppazlopez@gmail.com>
2026-02-15 12:57:29 -0300

    Move import statements for logging and settings inside config_loggers function for better encapsulation

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,9 +1,7 @@
 import os
-from logging.config import dictConfig
 
 from celery import Celery
 from celery.signals import setup_logging
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 app = Celery("apps")
@@ -12,6 +10,10 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 @setup_logging.connect
 def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig  # noqa: PLC0415
+
+    from django.conf import settings  # noqa: PLC0415
+
     dictConfig(settings.LOGGING)
 
 
